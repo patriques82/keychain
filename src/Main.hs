4a2b544542474984af11f7857b5c243fb9aed46c
Patrik Nygren <patriknygren82@gmail.com>
2018-01-13 15:03:22 +0100

    fixed Emils comments

--- src/Main.hs
+++ src/Main.hs
@@ -30,53 +30,33 @@ newtype App a = App {
   runApp :: ExceptT Error IO a  -- IO (Either Error a)
 } deriving (Functor, Applicative, Monad, MonadIO)
 
-data Command = SetupOrigin EncryptedFilePath PasswordFilePath
-             | SetupRemote EncryptedFilePath
-             | Add String String
-             | Key String
-             | Encrypt
-             | List
-             | Usage
-             | Help
-  deriving (Eq, Show)
-
 delim = ':'
 lockfile = ".lockfile"
 
 -- Main
 
 main :: IO ()
-main = getArgs >>= exec . parse
+main = getArgs >>= exec
 
-parse :: [String] -> Command
-parse ["setup", encrypted, "-p", passwords] = SetupOrigin encrypted passwords
-parse ["setup", encrypted]                  = SetupRemote encrypted
-parse ["add", site, key]                    = Add site key
-parse ["key", site]                         = Key site
-parse ["encrypt"]                           = Encrypt
-parse ["list"]                              = List
-parse ["-h"]                                = Help
-parse _                                     = Usage
-
-exec :: Command -> IO ()
-exec (SetupOrigin e p) = run (setupOrigin e p)
-exec (SetupRemote f)   = undefined
-exec (Add s k)         = getPassword >>= run . add s k
-exec (Key s)           = getPassword >>= run . key s
-exec Encrypt           = getPassword >>= run . encrypt
-exec List              = undefined
-exec Help              = undefined
-exec Usage             = undefined
+exec :: [String] -> IO ()
+exec ["setup", e, "-p", p] = run $ setupOrigin e p
+exec ["setup", e]          = undefined
+exec ["add", s, k]         = getPassword >>= run . add s k
+exec ["key", s]            = getPassword >>= run . key s
+exec ["encrypt"]           = getPassword >>= run . encrypt
+exec ["list"]              = undefined
+exec ["-h"]                = undefined
+exec _                     = undefined
 
 run :: App () -> IO ()
 run app = (runExceptT . runApp $ app) >>= either putStrLn return
 
 -- command semantics
 
-setupOrigin :: FilePath -> Password -> App ()
-setupOrigin encrypted password = do
+setupOrigin :: EncryptedFilePath -> PasswordFilePath -> App ()
+setupOrigin e p = do
   f <- configurationFile
-  c <- encrypted +++ password
+  c <- concatFilePaths e p
   liftIO $ writeFile f c
 
 -- TODO: incomplete
@@ -119,7 +99,7 @@ configFilePaths = do
     then mapM exist (split s)
     else throw "Empty config"
 
-decryptFile :: String -> BS.ByteString -> App Text
+decryptFile :: Password -> BS.ByteString -> App Text
 decryptFile p f = do
   let p' = pack p
   f' <- liftIO $ CBC.decrypt p' f
@@ -130,8 +110,8 @@ configurationFile = do
   cf <- flip (</>) lockfile <$> liftIO getHomeDirectory
   exist cf
 
-(+++) :: FilePath -> FilePath -> App String
-(+++) f p = do
+concatFilePaths :: FilePath -> FilePath -> App String
+concatFilePaths f p = do
   f' <- exist f
   p' <- exist p
   return $ f' ++ (delim:p')
@@ -147,7 +127,7 @@ throw :: Error -> App a
 throw s = App $ throwE s
 
 -- appends 0:s until 17 characters have been reached
-getPassword :: IO String
+getPassword :: IO Password
 getPassword = do
   putStrLn "enter password"
   l <- getLine
@@ -165,4 +145,3 @@ padR :: Int -> Char -> String -> String
 padR n c cs
   | n > 0 = cs ++ replicate n c
   | otherwise = cs
-
